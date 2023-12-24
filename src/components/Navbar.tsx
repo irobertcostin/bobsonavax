ad51fa3ffe43e767405764e6a96a23ab026c3c4c
irobertcostin <irobertcostin@gmail.com>
2023-12-24 23:47:26 +0100

    navbar revamp

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,40 +8,41 @@ const Navbar: React.FC = () => {
 
 
     return (
-        <div className="w-full relative  h-24 md:h-32 bg-amber-500 flex justify-end items-center p-4 md:px-10 lg:px-30">
+        <div className="w-full relative  h-20 md:h-32 bg-[#ee8c3a] flex justify-between items-center p-4 md:px-10 lg:px-30">
+
+            <img src={logo} alt="BOBS Logo" className=" cursor-pointer shadow-2xlr w-14 h-14 md:h-24 md:w-24 rounded-full border-2 border-black -bottom-10 md:-bottom-16 left-10 md:left-24 lg:left-60 lg:-bottom-24 hover:rotate-90 ease-in-out duration-300"></img>
 
 
-            <div className=" hidden justify-center items-center md:flex  gap-4 mr-4">
+            <div className="flex w-full items-center justify-end">
+                <div className=" hidden  justify-center items-center md:flex  gap-4 mr-4">
 
 
-                <a href="https://twitter.com/BOBSonAVAX" target="_blank" rel="noreferrer">
-                    <div className="w-14 h-14 border-2 border-white bg-black p-1 ">
-                        <img src={twitter} alt="Twitter Logo"></img>
-                    </div>
-                </a>
+                    <a href="https://twitter.com/BOBSonAVAX" target="_blank" rel="noreferrer">
+                        <div className="w-10 h-10 rounded-md  bg-black p-1 ">
+                            <img src={twitter} alt="Twitter Logo"></img>
+                        </div>
+                    </a>
 
 
-                <a href="https://t.me/avax_bobs" target="_blank" rel="noreferrer">
-                    <div className="w-14 h-14 border-2 border-white bg-black p-1">
-                        <img src={tg} alt="Telegram Logo"></img>
-                    </div>
-                </a>
+                    <a href="https://t.me/avax_bobs" target="_blank" rel="noreferrer">
+                        <div className="w-10 h-10 rounded-md  bg-black p-1">
+                            <img src={tg} alt="Telegram Logo"></img>
+                        </div>
+                    </a>
 
-                <a href="https://dexscreener.com/avalanche/0x8d59f25a2a31f80d12dda8d6cee125684ea1d019" target="_blank" rel="noreferrer">
-                    <div className="w-14 h-14   bg-black rounded-sm border-2 border-white">
-                        <img src={dexscreener} alt="Dexscreener Logo"></img>
-                    </div>
-                </a>
+                    <a href="https://dexscreener.com/avalanche/0x8d59f25a2a31f80d12dda8d6cee125684ea1d019" target="_blank" rel="noreferrer" >
+                        <div className="w-10 h-10 rounded-md   bg-black ">
+                            <img src={dexscreener} className="rounded-md" alt="Dexscreener Logo"></img>
+                        </div>
+                    </a>
+                </div>
+
+                <div className=" ">
+                    <button className="bg-black rounded-md  text-white px-3 py-2 hover:bg-slate-800 ease-in-out duration-300">Connect wallet</button>
+                </div>
             </div>
 
-            <a href="https://traderjoexyz.com/avalanche/trade?outputCurrency=0xf5f3216E9fed36F8cCf08D310FEc6FBf7f06200f" target="_blank" rel="noreferrer">
-                <div className="border-2 ">
-                    <button className="bg-black text-white p-3 font-semibold text-sm md:text-lg  ease-out duration-300 hover:-translate-x-1.5 hover:-translate-y-1.5">BUY SOME $BOBS</button>
-                </div>
-            </a>
 
-
-            <img src={logo} alt="BOBS Logo" className="absolute cursor-pointer z-50 w-20 h-20 md:h-32 md:w-32 lg:h-48 lg:w-48 rounded-full border-2 border-black -bottom-10 md:-bottom-16 left-10 md:left-24 lg:left-60 lg:-bottom-24 hover:rotate-90 ease-in-out duration-300"></img>
 
 
 
