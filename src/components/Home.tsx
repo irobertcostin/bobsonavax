0d09348ecdf152bf576c1567696c3169ea9e6b99
irobertcostin <irobertcostin@gmail.com>
2023-12-25 12:35:22 +0100

    excluded bobsnomics component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,13 +2,14 @@
 import Api from "../service/Api"
 import { useEffect, useState } from "react"
 import PairDexscreener from "./interfaces/PairInterface"
-// components
+
 import TypicalComponent from "./home-components/TypicalComponent"
 import Roadmap from "./home-components/Roadmap"
-// import Bobsnomics from "./home-components/Bobsnomics"
 import BuyComponent from "./home-components/BuyComponent"
 import Footer from "./Footer"
 import Statistics from "./home-components/Statistics"
+
+
 
 
 const Home: React.FC = () => {
@@ -31,13 +32,13 @@ const Home: React.FC = () => {
     }, [data])
 
     return (
-        <div className="w-full relative flex flex-col justify-center items-center" >
+        <div className="w-full bg-black relative flex flex-col justify-center items-center" >
 
 
 
             <TypicalComponent />
 
-            <div className="w-full lg:text-xl quick-desc">
+            <div className="w-full border-2 border-black lg:text-xl quick-desc">
                 <div className="bg-black w-full p-4">
                     <p className="text-center bg-[#ee8c3a] rounded-lg shadow-xl text-white md:text-lg lg:text-xl font-semibold px-4 py-10 lg:px-20 ">
                         Embracing the spirit of community hilarity, $BOBS is more than just a meme coin on the Avalanche network; it's a digital symphony of laughter and camaraderie. Created to infuse joy into the crypto space, $BOBS stands as a testament to the whimsical fusion of blockchain and meme culture.
@@ -45,7 +46,6 @@ const Home: React.FC = () => {
                 </div>
             </div>
             <Roadmap />
-            {/* <Bobsnomics /> */}
             <BuyComponent />
             <Statistics data={data} />
             <Footer />
